ece5a8e489da2a429c49422a9e7bee5923409997
logeek <2031500185@qq.com>
2022-10-04 15:00:21 +0800

    消息系统基本重构完成，主要增加了对系统通知的支持

---
+++ src/components/User/NickName.vue
@@ -0,0 +1,44 @@
+<template>
+    <span>{{ name ? name : "[用户不存在]" }}</span>
+</template>
+
+<script>
+
+import { getNickNameByUserId } from '@/api/user'
+
+export default {
+    name: "UserNickName",
+    components: {},
+    props: {
+        userId: {
+            type: Number,
+            default: null
+        }
+    },
+    data() {
+        return {
+            name: "-"
+        };
+    },
+    watch: {
+        userId: function(n, o) {
+
+        }
+    },
+    computed: {},
+    methods: {
+        getNickName(userId) {
+            getNickNameByUserId(userId).then(rep => {
+                this.name = rep.data.value
+            })
+        }
+    },
+    created() { },
+    mounted() { 
+        if(this.userId) this.getNickName(this.userId)
+    }
+};
+</script>
+<style scoped>
+
+</style>
